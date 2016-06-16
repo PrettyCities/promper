c2be87b7c0bfc83c4655813dc6c8f7e713d2248e
prettycities <mrcye@gmail.com>
2016-06-16 09:54:24 -0500

    Rearranged directories

--- lib/db.rb
+++ lib/db.rb
@@ -3,7 +3,7 @@ require 'YAML'
 module DataBase
   attr_reader :db
 
-  def initialize(db = YAML.load_file("db/master.yml"))
+  def initialize(db = YAML.load_file("master.yml"))
     @db = db
   end
 
--- lib/promper/version.rb
+++ lib/promper/version.rb
@@ -1,3 +1,3 @@
 class Promper
-  VERSION = "0.1.6"
+  VERSION = "0.1.7"
 end
