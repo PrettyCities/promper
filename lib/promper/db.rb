ad4e98696af5ed24c25f3200a8ac755df9c0d77e
prettycities <mrcye@gmail.com>
2016-06-16 10:27:16 -0500

    Experimenting with file requirements

--- lib/promper/db.rb
+++ lib/promper/db.rb
@@ -3,7 +3,7 @@ require 'YAML'
 module DataBase
   attr_reader :db
 
-  def initialize(db = YAML.load_file('promper/master.yml'))
+  def initialize(db = YAML.load_file(File.join(File.dirname(File.expand_path(__FILE__)), '../promper/master.yml')))
     @db = db
   end
 
